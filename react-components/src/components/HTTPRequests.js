81ea7d6ffd74b7e4d7abfa94c7c1312cd5152af5
Humberto Pereira <humberto_pereira@live.com>
2023-12-19 13:52:04 +0100

    commit

---
+++ react-components/src/components/HTTPRequests.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class HTTPRequests extends Component {
+    render() {
+        return (
+            <div>HTTPRequests</div>
+        )
+    }
+}
+
+export default HTTPRequests
